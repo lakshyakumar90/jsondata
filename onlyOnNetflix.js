b36e212d6b74263bc88477a0f361fae6474855e7
Lakshya Kumar <lakshyakumar5023@gmail.com>
2024-11-12 17:26:41 +0530

    Create onlyOnNetflix.js

---
+++ onlyOnNetflix.js
@@ -0,0 +1,113 @@
+export const onlyOnNetflixMovieData = [
+    {
+      id: 1,
+      imageId:
+        "https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABRY13rE6hFZeP981fDAgmKTcN4P0SzIOzCS8PMVNcm1A41O8G1dqia0SGeBUzBvFiH3JX_ugRAq5nbRkhsb4FpQy6PLwZyRw5cTO7EjbrO8YX7-Pk2joGpoaBQYi6RwFo4p11Q.webp?r=3f5",
+      name: "Money Heist",
+      nameImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABUAJPJHZ3EDzkFpq1SJZ3p6udZ6yTPxG2PzGsRsTh_UAJjJ1FDSWTh4hOumhxgDtWvWWDlwvmfjIpprd1G6mtE0GY6H2KRW4qQ.webp?r=768",
+      popImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABegE6h3opbXYRGkAc140FByhDJqs1h2w1M508Z4hP5OcI3VZL3pq-GUSWtTfvoJYppnJJd7f-OcKh_FLmZVBODAgmlrMz3_OMaYY.webp?r=ed3",
+      info: ["2021", "A", "Show", "Thrillers"],
+      description:
+        "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police to carry out his plan.",
+    },
+    {
+      id: 2,
+      imageId:
+        "https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABW-d7ZXaidP5ykOiy-MSmgy-yVF1C4_68_poOkx-ZU04wLVevpoqkf3uEvHCsGZY_XipOSlNASQ0TfqWvjspCLE5-2AIVHHQY29oJC62Yd8PhSLBaZqzreyhaAmAM6Z7YQBBMg.webp?r=e79",
+      name: "Stranger Things",
+      nameImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABWCbEzpdkYL3YFByruGPW5g9h5GUnQI8cUijxlMF53i5kZYUzz6mOt0QN37RpbjdqWQhY3D6kXLdhPqrBj6r7Ij2PFYeO-C19g.webp?r=23b",
+      popImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABcICtm-83lqIApMjAA1zyFZ3lYGiiJa8_SMHAUnzYXrzX4LPALBjX3iwyc7ZqaQDeyKTgZaZwW7n0Y7kV30hHJRH2q7l7nOjtoCh.webp?r=611",
+      info: ["2022", "U/A 16+", "Show", "Horror"],
+      description:
+        "When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces and one strange little girl.",
+    },
+    {
+      id: 3,
+      imageId:
+        "https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABQ26mhDTxFE58n5Cwgwe_ReId62_vwDTsFu8zNv9aO1OW6SWAbCeGIN_th8eHMv2sdDSY1rk6STdF7r2GGLshF5ID25njhBE0IeNcasFwPxHlF33NAPTXgZOCOX_NhMUGbXnBg.webp?r=c7b",
+      name: "Queen of Tears",
+      nameImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABRjUDjQiCM1Trm2uhmVdz0jXncG2ZbMAjEb7fiC1IIn5gfIrF8hHtk3n-qKou1ZxXRV2ZwIGDNUOWrXFySiJ5dUQ_uDZa4VHEQ.webp?r=20a",
+      popImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABfslWE-UFqOFT7kHcTPhAyxUgDZh830CEZGgF5w7rKit6-Sc-5Dl7PZr6I0v6j0UvHVaRqUuzQO6VkyqYxyMZMobFMIkfA86q2Cr.webp?r=46e",
+      info: ["2024", "U/A 13+", "Show", "Comedies"],
+      description:
+        "The queen of department stores and her small-town husband weather a marital crisis —until love miraculously begins to bloom again.",
+    },
+    {
+      id: 4,
+      imageId:
+        "https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABROJMgHuT0kF6UHU-1PgR5T6_MyUWbqAN8a70TMKBaTV6TDEif7AKv6BARn67_U4ZlVx31vWny9ljXaa74-HLXs3UD35gcXbnUcT56Bk9uqrh9jsBYaP-FGfSV8COqgJIyrNEg.webp?r=112",
+      name: "Lucifer",
+      nameImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABU5VT_sBS8YTKx2Wd9esQt2YQUsVmTaLFJtkxCrpS0GtVx0NmL1ovG6z_g66xC7yhFa5N0v1fEvBF3m8EkI0Krwu1I4x4t78ug.webp?r=da2",
+      popImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABR_Po8_Q0uxiZH6RzKDG6uW2LnSszBQ3NDoJJLtu-sMjzCFvqFxrA8viW9_KKpy1ohdb-dvczflJMcljsao2IJP1R4bhvdo03a4w.webp?r=11c",
+      info: ["2021", "U/A 16+", "Show", "Fantasy"],
+      description:
+        "Bored with being the Lord of Hell, the devil relocates to Los Angeles, where he opens a nightclub and forms a connection with a homicide detective.",
+    },
+    {
+      id: 5,
+      imageId:
+        "https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABQ0U16Omm3NmKivD23lnV3pX3wJkmKXSAFUX5yGe19twNnPnqyU9bW8JtGsM3AP6UNqlb6MA4OBboVR7gC8FuZJxqtNjaPaheZMcgpFHzKzayydDgZ5_31sfw5lwoyFZsxJ8sA.webp?r=e3c",
+      name: "My Demon",
+      nameImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABS-DCDDZCSrHk0LN___zMnQdTClLn1en1FJ_hSRjhJv0Nj7P15nqMAI-xCQ5ATER2AmHZk-kl5CSs2YxZPnVTA1tUR-luWck7w.webp?r=bc3",
+      popImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABdQWhDuqDQ8Zs85eyoO84P31FUjWuEsNSfuuczY7fbTDlJ-wbIcyRb_dYeYXkui2ID0zbHowPsvqZuEVxP5a6epiZdTPA74XfGU_.webp?r=d25",
+      info: ["2023", "U/A 16+", "Show", "Comedies"],
+      description:
+        "A pitiless demon becomes powerless after getting entangled with an icy heiress, who may hold the key to his lost abilities — and his heart.",
+    },
+    {
+      id: 6,
+      imageId:
+        "https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABT3mYMgjE3-02zLGOzNVq0bMKftmnuK3dN3uIcmxQosihlsta4ZVfkBdiNeHABPECoer7HGSR0pi4W0JbBgLGAxjoJMT8qp3VWvwCYBVV29xufw7DM__Cm0VMDJwP7TngxahVw.webp?r=c37",
+      name: "The Lincoln Lawyer",
+      nameImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABd5Qebjck2fGVA2122m3Okqubv7rJC2W0knVGfqWWWQHYXmXQPos9-ixJbbPoQkP8COnITrlRNV3iq-oSyA58EluArAI6V1t1DQ.webp?r=0b9",
+      popImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABTQKz6Sq8Tx8TLeAFeCPePAT2xODQLFQlVtv3PD8sK_9h8XxYNK0feXzJT2R1rOoJS--iHYUSZiclM1fg589tA6J3DL2AF9UfYad.webp?r=7c9",
+      info: ["2024", "U/A 16+", "Show", "Thrillers"],
+      description:
+        "Hotshot LA defense attorney Mickey Haller will do whatever it takes to win as he navigates the criminal justice system from his trademark Lincoln.",
+    },
+    {
+      id: 7,
+      imageId:
+        "https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABcBfQ5g7suBqQ1OfrnFBRfK_3cXnGqyVcpAOUM28hHX0mWh8kzzP0kCmrNZFszZ-GukuXVJiK8tGbw6f5AM4zaYYXYvmRk6hXQZrXxswq7c18th-3TvNDTSkG3_EKi-yaZWfQg.webp?r=a0c",
+      name: "Heeramandi",
+      nameImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABbUAFfPHzXP5Kjeno3qMuQkv2t0dfNgxDLTxZUkMUtLHhoXxy6zCRSusbZm5uFuXC2P4IH_jjbPEdG5BHTF8q3ade13xi4kkwA.webp?r=ff5",
+      popImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABV16AndUJYuKOrMZyG6GyDAl2TTpkvU7YZzUMg-zJs7dxU-Iei5T1HLA5OfNpSM54fEv23yW-LyYO5b9L3uePk7hLq2YKoZWfsuL.webp?r=182",
+      info: ["2024", "U/A 16+", "Show", "Romance"],
+      description:
+        "The scheming Mallikajaan rules over an elite house of courtesans — but a new rival threatens her reign as rebellion brews in British-ruled India.",
+    },
+    {
+      id: 8,
+      imageId:
+        "https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABcthhZIohsu0feEvCd4hCmcRUJ7wP3fSN1eBFLvYLO1E56hGa9dTJus3OZ8lyDSQxvaQNRgPWdZl58_S2BFIvPOEdJgKfhDatMWH8zOJRFTaukJ5FJRa1HutRInOIPCr6wsjLg.webp?r=e07",
+      name: "IC814 - The Kandahar Hijack",
+      nameImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABSD6EBsuHH55lTOhKzVUCHiCVY6fK3wsCUEMoKfnZh7HZdf0V7eqGBEyBD66fMY-u6ZbGCx_GJ_TX3zWrKRsvaXiDIy9T3os-rE.webp?r=a35",
+      popImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABf61JkOWiD3b-HkwGwgrcufN0WkeouW1fvrZ4yIbWCI0zoKbZa_LegGsCYAW1TFSQ3zQHxUTyQUY7C43IBKB6-H7R0aW7mpCjcL_.webp?r=54c",
+      info: ["2024", "U/A 16+", "Show", "Thrillers"],
+      description:
+        "When IC 814 gets hijacked on its way to Delhi, hundreds of lives are at stake as the country faces its longest and most alarming aviation crisis.",
+    },
+    {
+      id: 9,
+      imageId:
+        "https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABXZXsO1ZHNELPr8ZpJYk0z5GN20zYLKybruSzhhblUs8z0m0nvLfSrPuRvS_ndtUwv4-oewKpbZRZvslZS2ZypvEWYQ5z-LvsvBj5dFMrMTquSOSzRleP7rcgRIsIWDFVZi2vw.webp?r=71f",
+      name: "Maharaj",
+      nameImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABU7Hj6U2CIplczWnT6bmnktDtPbW_VKVTv0mL3mGZ2A4Hav-OohJyj5OEGjBF7mXaIzj99TLYo0D8DIG97HZiuWd6Oz3YhkteA.webp?r=7de",
+      popImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABVb1usV5LZpVOHAxk2ZF2sIYUIOZVBVgV1Gmj8PwqntQ2AChiv2HGCxu5iM1yh8Flt6WwOkwj_HWYcLhlSYWvaORUGeqvdbokDlE.webp?r=bdb",
+      info: ["2024", "U/A 16+", "Movie", "Dramas"],
+      description:
+        "Based on a real-life historic court case, a bold journalist questions a revered leader’s immoral behavior.",
+    },
+    {
+      id: 10,
+      imageId:
+        "https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABdbKfm_grERo569cMP2buiT-mBuh9jLLONiiVaYHGR6E6BeZyBjRUdPEDvy8j8ZcPU_yVwBM6pL1tNBFDyjoR93O6yfBk0pUGn8swBrdsmbLVKM3SfA6q_onC0QRpTVvFsDa3Q.webp?r=e73",
+      name: "Tribhuvan Mishra CA Topper",
+      nameImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABRykH0tfsYeqHylGgQkBrIeURLEbS4MqErkjYMKOPtlp4JqMAdfMCZoDj8t1nVAZJXz8AQJTuelqIOG7BrzxxONnWsI9Vt9Dlg.webp?r=be7",
+      popImg:"https://occ-0-2483-2164.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABUEXqwFXChPYcqrMBd-d2q0NI9PmlTMbzQOnI55coM_sW1zJFpVbKHmFvyAFaSYNVDpLB1uzcCmUv9lXKfdj3akiNcts-7BPMBV1.webp?r=ec0",
+      info: ["2024", "A", "Show", "Comedies"],
+      description:
+        "A banking crisis forces CA topper Tribhuvan Mishra to take up sex work. Will this dual existence alleviate his troubles — or create entirely new ones?",
+    },
+  ];
+  
